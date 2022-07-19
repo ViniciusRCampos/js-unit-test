72401d725fed61b4511fe21d15002fed88e843f9
ViniciusRCampos <vinicius_uo@hotmail.com>
2022-07-19 19:38:06 -0300

    Requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,14 +30,33 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+  // it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+  //   fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+  it('Teste se productDetails é uma função', () => {
+    expect(typeof(productDetails)).toBe('function')
+  })
     // Teste se o retorno da função é um array.
+  // it('Teste se o retorno da função é um array.', () => {
+  //   expect(typeof(productDetails('alcool','mascara'))).toBe('array');
+  // })
     // Teste se o array retornado pela função contém dois itens dentro.
+  it('Teste se o array retornado pela função contém dois itens dentro.', () => {
+    expect(Object.keys(productDetails('alcool','mascara'))).toHaveLength(2)
+  })
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+  it('Teste se os dois itens dentro do array retornado pela função são objetos.', () => {
+    expect(typeof(productDetails('alcool','mascara')[0])).toBe('object')
+    expect(typeof(productDetails('alcool','mascara')[1])).toBe('object')
+  })
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+  it('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
+    expect((productDetails('alcool','mascara')[0])).not.toBe(productDetails('alcool','mascara')[1]);
+  })
     // Teste se os dois productIds terminam com 123.
+  it('Teste se os dois productIds terminam com 123.', () => {
+    expect(productDetails('alcool','mascara')[0].details.productId.endsWith('123')).toBe(true);
+    expect(productDetails('alcool','mascara')[1].details.productId.endsWith('123')).toBe(true);
+  })
   });
-});
